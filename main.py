079b4583a12f0776e77c3df60de7bb300ae6e0f1
mackbr96 <mackbr@clarkson.edu>
2018-12-09 14:55:05 -0500

    Ready for take off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sys
 
 def parse(obs, emit, chain):
     bias = []
@@ -34,9 +34,34 @@ def parse(obs, emit, chain):
         print(bias)
         return input, np.array(matrix, dtype=float), np.array(bias, dtype=float)
 
-obsv, matrix, bias = parse("obs-1.txt", "emit-1.txt", "chain-1.txt")
-#obsv, matrix, bias = parse("obs-2.txt", "emit-2.txt", "chain-2.txt")
-#obsv, matrix, bias = parse("testobs.txt","testemit.txt", "testchain.txt")
+def findProbSeq(obsv, matrix, bias):
+    print("\n\n")
+    totalprob = 0.4
+
+    obsv = obsv[1:]
+    x = bias[0]
+    y = bias[1]
+    if(obsv[0] == "1"):
+        prev = np.argmax(bias)
+    else:
+        prev = np.argmin(bias)
+    for i in obsv:
+        if(i == "1"):
+            prob = (max(matrix[prev]) * bias)
+            totalprob = totalprob * (bias[np.argmax(prob)])
+            prev = np.argmax(prob)
+        else:
+            prob = (max(matrix[prev]) * (100 - bias))
+            totalprob = totalprob * (1 - bias[np.argmax(prob)])
+            prev = np.argmax(prob)
+    print(totalprob)
+    
+
+
+if(len(sys.argv) == 4):
+    obsv, matrix, bias = parse(sys.argv[1], sys.argv[2], sys.argv[3])
+else:
+    obsv, matrix, bias = parse("testobs.txt","testemit.txt", "testchain.txt")
 
 with open("output.txt", "w") as f:
 
@@ -48,6 +73,7 @@ with open("output.txt", "w") as f:
     else:
         prev = np.argmin(bias)
     print(str(prev) + " " + str(obsv[0]))
+    f.write(str(prev) + " ")
     for i in obsv:
         if(i == "1"):
             prob = (max(matrix[prev]) * bias)
@@ -55,64 +81,10 @@ with open("output.txt", "w") as f:
             f.write(str(np.argmax(prob)) + " ")
             prev = np.argmax(prob)
         else:
-            prob = (max(matrix[prev]) * (100 - bias))
+            prob = (max(matrix[prev]) * (1 - bias))
             print(str(np.argmax(prob)) + " " + str(i))
             f.write(str(np.argmax(prob)) + " ")
             prev = np.argmax(prob)
     f.close()
 
-
-# for i in obsv:
-#     if(i == "H"):
-#         if(prev == "X"):
-#             #probX = (matrix[0][0] * x) / x
-#             #probY = (matrix[0][1] * y) / y
-#             probX = (max(matrix[0]) * bias)
-#             if(probX[0] > probX[1]):
-#                 print("X " + "H")
-#                 prev = "X"
-#             else:
-#                 print("Y " + "H")
-#                 prev = "Y"
-
-#         else:
-#             # probX = (matrix[1][0] * x) / x
-#             # probY = (matrix[1][1] * y) / y
-#             probX = (max(matrix[1]) * (bias))
-#             if(probX[0] > probX[1]):
-#                 print("X " + "H")
-#                 prev = "X"
-#             else:
-#                 print("Y " + "H")
-#                 prev = "Y"
-
-
-#     else:
-#         if(prev == "X"):
-#             #probX = (matrix[0][0] * (100-x)) / (100-x)
-#             #probY = (matrix[0][1] * (100-y)) / (100-y)
-#             probX = (max(matrix[0]) * (100-bias))
-#             if(probX[0] > probX[1]):
-#                 print("X " + "T")
-#                 prev = "X"
-#             else:
-#                 print("Y " + "T")
-#                 prev = "Y"
-
-#         else:
-#             # probX = (matrix[1][0] * (100-x)) / (100-x)
-#             # probY = (matrix[1][1] * (100-y)) / (100-y)
-#             probX = (max(matrix[1]) * (100-bias))
-#             if(probX[0] > probX[1]):
-#                 print("X " + "T")
-#                 prev = "X"
-#             else:
-#                 print("Y " + "T")
-#                 prev = "Y"
-
-        
-
-            
-
-
-
+#findProbSeq("111", matrix, bias)
